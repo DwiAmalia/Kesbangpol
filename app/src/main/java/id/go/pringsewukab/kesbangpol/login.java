f890089b244d3c23a64e7df844f79aafe6890ced
Dwi Amalia <dwiamlx@gmail.com>
2023-03-10 11:04:06 +0700

    rev1

---
+++ app/src/main/java/id/go/pringsewukab/kesbangpol/login.java
@@ -0,0 +1,14 @@
+package id.go.pringsewukab.kesbangpol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
