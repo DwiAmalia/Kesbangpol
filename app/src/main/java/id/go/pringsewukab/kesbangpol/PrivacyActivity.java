a5a17a068fc29869b3a88d25ce9f7092ae25f6c3
Dwi Amalia <dwiamlx@gmail.com>
2023-03-10 14:27:32 +0700

    rev privacy activity fix

---
+++ app/src/main/java/id/go/pringsewukab/kesbangpol/PrivacyActivity.java
@@ -0,0 +1,24 @@
+package id.go.pringsewukab.kesbangpol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import id.go.pringsewukab.kesbangpol.databinding.ActivityPrivacyBinding;
+
+public class PrivacyActivity extends AppCompatActivity {
+    private ActivityPrivacyBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+        getSupportActionBar().hide();
+
+        binding = ActivityPrivacyBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+    }
+}
