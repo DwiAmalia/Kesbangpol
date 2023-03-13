ce39cd2070e68aef1ebdb0297eb8b796b5c1172e
Dwi Amalia <dwiamlx@gmail.com>
2023-03-13 11:29:01 +0700

    rev login

--- app/src/main/java/id/go/pringsewukab/kesbangpol/login.java
+++ app/src/main/java/id/go/pringsewukab/kesbangpol/login.java
@@ -1,14 +1,64 @@
 package id.go.pringsewukab.kesbangpol;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class login extends AppCompatActivity {
+    TextView tvForgetPass;
+    TextInputEditText inputUsername, inputPassword;
+    Button btnLogin;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvForgetPass = findViewById(R.id.tvForgetPassword);
+        tvForgetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDiaglog = new AlertDialog.Builder(login.this);
+                alertDiaglog.setMessage("Lupa Password Harap Menghubungi Kontak Developer");
+                alertDiaglog.setPositiveButton("Oke", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                alertDiaglog.show();
+            }
+        });
+
+        btnLogin = findViewById(R.id.login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(inputUsername.getText().toString())||
+                TextUtils.isEmpty(inputPassword.getText().toString())){
+                    AlertDialog.Builder alertDiaglog = new AlertDialog.Builder(login.this);
+                    alertDiaglog.setMessage("Harap Masukkan Username dan Password");
+                    alertDiaglog.setPositiveButton("Oke", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    alertDiaglog.show();
+                }
+            }
+        });
+
     }
 }
