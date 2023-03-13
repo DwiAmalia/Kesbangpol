59302c031fd8c046623a6cb27662a914ffd8fd42
Dwi Amalia <dwiamlx@gmail.com>
2023-03-13 09:42:07 +0700

    rev2

---
+++ app/src/main/java/id/go/pringsewukab/kesbangpol/ui/ChangePassword.java
@@ -0,0 +1,16 @@
+package id.go.pringsewukab.kesbangpol.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.go.pringsewukab.kesbangpol.R;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
--- app/src/main/java/id/go/pringsewukab/kesbangpol/ui/fragment/InformationAppsFragment.java
+++ app/src/main/java/id/go/pringsewukab/kesbangpol/ui/fragment/InformationAppsFragment.java
@@ -1,25 +1,39 @@
 package id.go.pringsewukab.kesbangpol.ui.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import id.go.pringsewukab.kesbangpol.R;
+import id.go.pringsewukab.kesbangpol.ui.PrivacyActivity;
 
 public class InformationAppsFragment extends Fragment {
+    ImageButton btnUbahPassword;
 
 
+    @SuppressLint("MissingInflatedId")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+
+       //Button or pop up
+       btnUbahPassword = view.findViewById(R.id.buttonUbahPassword);
+       btnUbahPassword.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getContext(), PrivacyActivity.class);
+               startActivity(intent);
+           }
+       });
+
+
        return view;
-    }
-
-
-    public void onClick(View view) {
     }
 }
